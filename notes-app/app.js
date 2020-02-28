4ba0824a2b3769f64376f59a065bf5ec50996f52
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-02-28 08:22:16 +0700

    Importing Nodejs Modules

---
+++ notes-app/app.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', "Created by Node.js!");
+
+fs.appendFileSync('notes.txt', " I am Thien. I come from Viet Nam.")
+// challenge: append a message to notes.txt
+//
+// 1. use appendFileSync to append to the file
+// 2. run the script
+// 3. check your work by opening the file and viewing the appended text
