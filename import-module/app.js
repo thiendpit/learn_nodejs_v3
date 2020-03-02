60260ab4bc461bfb1618d3fca96b280587c0b39e
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-03-02 10:00:38 +0700

    Print color with chalk module

--- import-module/app.js
+++ import-module/app.js
@@ -1,4 +1,10 @@
-const validator = require("validator");
+// const validator = require("validator");
 
-console.log(validator.isEmail('abc@aaa.com'));
-console.log(validator.isDecimal('a'));
+// console.log(validator.isEmail('abc@aaa.com'));
+// console.log(validator.isDecimal('a'));
+
+const chalk =  require("chalk");
+
+console.log(chalk.green('Success!')); // print green color
+console.log(chalk.bold.green('Success!')); // print green color and bold
+console.log(chalk.italic.green('Success!')); // print green color and italic
